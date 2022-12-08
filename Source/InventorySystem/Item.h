d762e4db40696ce2f912dbeff2e8004c36ae5440
Cade Gordon <cadegordon093@gmail.com>
2022-12-08 14:34:39 -0600

    added a struct file for data structure

--- Source/InventorySystem/Item.h
+++ Source/InventorySystem/Item.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "InteractableInterface.h"
+#include "Structs.h"
 #include "Item.generated.h"
 
 UCLASS()
@@ -20,6 +21,9 @@ protected:
 
 	UPROPERTY(EditDefaultsOnly, Category = "Item")
 	class UStaticMeshComponent* ItemMesh;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Item")
+	FItemData ItemData;
 
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
